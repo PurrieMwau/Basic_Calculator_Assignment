fe8375a9f41bc4e9833a6e87917cfc68f0ae8b0b
PurrieMwau <puritymbula07@gmail.com>
2025-08-04 18:57:21 +0300

    Basic_Calculator

---
+++ Basic_Calculator.py
@@ -0,0 +1,19 @@
+a = float(input("Enter the first number: "))
+
+b = float(input("Enter the second number: "))
+
+sum_result = a + b
+
+difference_result = a - b
+
+product_result = a* b
+
+quotient_result = a / b
+
+print(f"Results of your two numbers:")
+print(f"Sum: {sum_result}")  
+print(f"Difference: {difference_result}")  
+print(f"Product: {product_result}")  
+print(f"Quotient: {quotient_result}")  
+print("Error: Cannot divide by zero")
+
